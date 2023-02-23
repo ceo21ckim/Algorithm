fcdfd1f34f8cb065778b199df9232ce1d9c32b72
Dongeon Kim <60685175+ceo21ckim@users.noreply.github.com>
2023-02-23 12:44:37 +0900

    Add files via upload

---
+++ Leetcode/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,37 @@
+'''
+You are given an array `prices` where `price[i]` is the price of a gvien stock on the `i-th` day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return `0`.
+
+Input: prices = [7, 1, 5, 3, 6, 4]
+Output: 5
+Explanation: Buy on day 2 (Price = 1) and sell on day 5 (Price = 6), prifit = 6 - 1 = 5
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+'''
+
+# time out
+prices = [7, 1, 5, 3, 6, 4]
+
+def maxProfit(prices):
+    if len(prices) == 1:
+        return 0 
+    
+    results = [max(prices[i+1:]) - prices[i] for i in range(len(prices)) if i < len(prices)-1]
+    outs = max(results)
+    return outs if outs > 0 else 0
+
+maxProfit(prices)
+
+
+# solution 1
+import sys 
+def maxProfit(prices):
+    profit = 0 
+    min_price = sys.maxsize # 9223372036854775807
+    
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = max(profit, price - min_price)
+    
+    return profit 
